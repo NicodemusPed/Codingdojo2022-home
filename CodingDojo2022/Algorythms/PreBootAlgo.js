c3b4328cc0919a2d0233b24603d1e56fe91315c6
NicodemusPed <107951768+NicodemusPed@users.noreply.github.com>
2022-07-03 14:58:25 -0700

    up

--- CodingDojo2022/Algorythms/PreBootAlgo.js
+++ CodingDojo2022/Algorythms/PreBootAlgo.js
@@ -1,8 +1,20 @@
 function counter() {    
         for (var num = 0; num <= 5; num++) {        
             console.log(num);    
-        }node
+        }
     }
     counter();    // run the function
     counter();    // run the function again
     
+
+    var x = 'coding';
+    var y = 'dojo';
+
+    for(var num1=0; num1<=x.lentgh-3; num1++)
+    {
+        for(var num2=0; num2<=y.length-4; num2++)
+
+        {
+            console.log(x+y);
+        }
+    }
---
+++ CodingDojo2022/Algorythms/app.js
@@ -0,0 +1,3 @@
+var msg = 'helo world';
+console.log(msg)
+msg.charAt
